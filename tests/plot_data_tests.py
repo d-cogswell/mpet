30a1c0dc0bc680c371dc44f12fd04668d255c28e
Dan Cogswell <cogswell@mit.edu>
2024-05-16 22:00:09 -0400

    Adds a plot_data test workflow.

---
+++ tests/plot_data_tests.py
@@ -0,0 +1,20 @@
+import pytest
+import os.path as osp
+import matplotlib.pyplot as plt
+import mpet.plot.plot_data as plot_data
+
+plots = list(plot_data.plotTypes.keys())
+plots.remove("text")
+
+
+@pytest.mark.parametrize("plot", plots)
+def test_plot(Dirs, plot):
+    refDir, testDir = Dirs
+    try:
+        plot_data.show_data(osp.join(testDir, "sim_output"), plot)
+    except NotImplementedError:
+        pass
+    except Exception:
+        assert False
+    plt.close()
+    assert True
